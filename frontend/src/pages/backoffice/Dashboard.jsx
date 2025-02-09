ac2067beccd63c1caaeb1640e03cfde70e7b444e
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-02-09 12:30:46 +0100

    Dashboard component update

--- frontend/src/pages/backoffice/Dashboard.jsx
+++ frontend/src/pages/backoffice/Dashboard.jsx
@@ -59,6 +59,7 @@ export default function Dashboard() {
                             key={index}
                             title={carro.title}
                             votes={carro.totaleVoti}
+                            img={carro.image}
                             onDelete={() => deleteVotes({carroId: carro._id})}
                         />
                     )
