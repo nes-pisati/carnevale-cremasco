96f2001cee874775eb995984eb1aed0014c3bfe4
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-02-09 12:30:21 +0100

    dashboard card update

--- frontend/src/components/DashboardCard/DashboardCard.jsx
+++ frontend/src/components/DashboardCard/DashboardCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function DashboardCard({ title, votes, onDelete }) {
+export default function DashboardCard({ title, votes, img, onDelete }) {
     return (
         <>
             <div className="w-96 sm:w-60 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col h-full min-h-[320px]">
-                <img className="rounded-t-lg h-40 object-cover w-full" src="https://picsum.photos/200/300" alt="" />
+                <img className="rounded-t-lg h-40 object-cover w-full" src={img} alt="Fotografia del carro allegorico" />
                 <div className="p-5 flex flex-col gap-4 flex-grow">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white leading-none">{title}</h5>
                     <div className="mt-auto flex justify-between items-center">
